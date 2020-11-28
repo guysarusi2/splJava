9498ee2b5682ad99519730473a84992338c2bc78
guysarusi2 <guysarusi2@gmail.com>
2020-11-28 22:22:58 +0200

    test

---
+++ src/main/java/bgu/spl/mics/SimpleMicroService.java
@@ -0,0 +1,14 @@
+package bgu.spl.mics;public class SimpleMicroService extends MicroService {
+    /**
+     * @param name the micro-service name (used mainly for debugging purposes -
+     *             does not have to be unique)
+     */
+    public SimpleMicroService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void initialize() {
+        return;
+    }
+}
